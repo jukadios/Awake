bd9f79fd3172e2604a93d39e5f0c8dad65b04024
Jukari <jukari98@gmail.com>
2022-02-28 11:05:54 -0600

    Player movement

--- Awake/Assets/Scripts/Player.cs
+++ Awake/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour {
 
@@ -54,5 +55,11 @@ public class Player : MonoBehaviour {
 
         vel.y = yvel;
         controller.Move(vel * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        
+
+        
     }
 }
